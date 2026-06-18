751e3813d512ec2053b320f5fb589fecaa64b26b
UkohEmmanuel1 <emmanuelukoh08@gmail.com>
2026-06-18 17:15:48 +0100

    remove

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -5,7 +5,6 @@ import {
   DifferentiatorsSection,
   ValuesSection,
   LeadershipSection,
-  GlobalFootprintSection,
 } from "@/components/sections/about";
 import { FinalCTA } from "@/components/common";
 
@@ -24,7 +23,6 @@ export function AboutPage() {
         <DifferentiatorsSection />
         <ValuesSection />
         <LeadershipSection />
-        <GlobalFootprintSection />
         <FinalCTA />
       </div>
     </main>
